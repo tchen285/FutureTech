cf67ea245a3613b72e2a423b62a284c3b6f979e2
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-11-24 13:44:51 -0800

    currently, 6 can be distributed as promised.

--- balancing.py
+++ balancing.py
@@ -14,7 +14,8 @@ class FindBalancingPath:
     def solve_balancing(self):
         while self.queue:
             # current_matrix, current_path = self.queue.popleft()
-            current_matrix = self.queue.popleft()
+            popped_matrix = self.queue.popleft()
+            current_matrix = popped_matrix
 
             # print("Current Matrix:")
             # for row in current_matrix:
@@ -29,10 +30,10 @@ class FindBalancingPath:
             # visited_hash = hash(str(current_matrix))  # 使用哈希值来避免重复访问相同状态
             # if visited_hash in self.visited:
             #     continue
-            if frozenset(map(frozenset, current_matrix)) in self.visited:
+            matrix_tuple = tuple(tuple(row) for row in current_matrix)
+            if matrix_tuple in self.visited:
                 continue
-            self.visited.add(frozenset(map(frozenset, current_matrix)))
-
+            self.visited.add(matrix_tuple)
 
             for col1 in range(self.cols):
                 if current_matrix[-1][col1] == 0:
@@ -43,15 +44,21 @@ class FindBalancingPath:
                         current_matrix[row1][col1] = 0
                         for col2 in range(self.cols):
                             for row2 in reversed(range(self.rows)):
-                                if current_matrix[row2][col2] == 0 and row2 != 0:
+                                if current_matrix[row2][col2] == 0 and col2 != col1:
                                     current_matrix[row2][col2] = weight
-                                    print(current_matrix)
-                                    if frozenset(map(frozenset, current_matrix)) in self.visited:
+                                    print(current_matrix[0])
+                                    print(current_matrix[1])
+                                    print("--------------")
+                                    matrix_tuple = tuple(tuple(row) for row in current_matrix)
+                                    if matrix_tuple in self.visited:
                                         continue
-                                    self.visited.add(frozenset(map(frozenset, current_matrix)))
+                                    self.visited.add(matrix_tuple)
+                                    break  # exit the loop for row2 once the condition is met
 
-                                    print("Can you see me?")
-                                    print(current_matrix)
+                            # Continue with the rest of the col2 loop
+                            current_matrix[row2][col2] = 0
+
+
 
             # for row in range(self.rows):
             #     for col in range(self.cols):
