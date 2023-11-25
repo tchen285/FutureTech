76b80e42fcbdc3b705b098b87dcc9daa4f42ecf5
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-11-24 22:29:00 -0800

    made some progress, need to deal with queue level by level tmr

--- balancing.py
+++ balancing.py
@@ -1,5 +1,6 @@
+import queue
 from collections import deque
-
+import copy
 
 class FindBalancingPath:
     def __init__(self, matrix):
@@ -13,23 +14,9 @@ class FindBalancingPath:
 
     def solve_balancing(self):
         while self.queue:
-            # current_matrix, current_path = self.queue.popleft()
             popped_matrix = self.queue.popleft()
             current_matrix = popped_matrix
 
-            # print("Current Matrix:")
-            # for row in current_matrix:
-            #     print(row)
-
-            # if self.is_balanced(current_matrix):
-            #     print("Balancing path found:")
-            #     for move in current_path:
-            #         print(move)
-            #     return
-
-            # visited_hash = hash(str(current_matrix))  # 使用哈希值来避免重复访问相同状态
-            # if visited_hash in self.visited:
-            #     continue
             matrix_tuple = tuple(tuple(row) for row in current_matrix)
             if matrix_tuple in self.visited:
                 continue
@@ -46,32 +33,25 @@ class FindBalancingPath:
                             for row2 in reversed(range(self.rows)):
                                 if current_matrix[row2][col2] == 0 and col2 != col1:
                                     current_matrix[row2][col2] = weight
+                                    print("\n--------------")
                                     print(current_matrix[0])
                                     print(current_matrix[1])
-                                    print("--------------")
+                                    print("--------------\n")
                                     matrix_tuple = tuple(tuple(row) for row in current_matrix)
                                     if matrix_tuple in self.visited:
                                         continue
                                     self.visited.add(matrix_tuple)
-                                    break  # exit the loop for row2 once the condition is met
 
-                            # Continue with the rest of the col2 loop
-                            current_matrix[row2][col2] = 0
+                                    # Append a copy of the matrix to the queue
+                                    self.queue.append(copy.deepcopy(current_matrix))
 
+                                    print("Queue Size:", len(self.queue))
+                                    print("Queue Elements:")
+                                    for elem in self.queue:
+                                        print(elem)
 
-
-            # for row in range(self.rows):
-            #     for col in range(self.cols):
-            #         for move_row in [-1, 0, 1]:
-            #             for move_col in [-1, 0, 1]:
-            #                 if move_row == 0 and move_col == 0:
-            #                     continue  # Skip the case where there is no movement
-            #
-            #                 new_matrix = self.make_move(current_matrix, row, col, move_row, move_col)
-            #                 if new_matrix:
-            #                     new_path = current_path + [(row, col, move_row, move_col)]
-            #                     self.queue.append((new_matrix, new_path))
-
+                                    current_matrix[row2][col2] = 0
+                                    break  # 只跳出当前循环
 
 
     def is_balanced(self, matrix):
@@ -103,7 +83,6 @@ def main():
 
     balancing_path_finder = FindBalancingPath(matrix)
     balancing_path_finder.solve_balancing()
-
 
 if __name__ == "__main__":
     main()
