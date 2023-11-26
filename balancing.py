1bfa649397f00ecfb1979f270410ee9d4840e4fb
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-11-25 17:39:55 -0800

    currently, the original_matrix is successfully passed

--- balancing.py
+++ balancing.py
@@ -14,52 +14,78 @@ class FindBalancingPath:
 
     def solve_balancing(self):
         while self.queue:
-            popped_matrix = self.queue.popleft()
-            current_matrix = popped_matrix
+            level_size = len(self.queue)
+            for _ in range(level_size):
+                popped_matrix = self.queue.popleft()
+                current_matrix = [row[:] for row in popped_matrix]
 
-            matrix_tuple = tuple(tuple(row) for row in current_matrix)
-            if matrix_tuple in self.visited:
-                continue
-            self.visited.add(matrix_tuple)
-
-            for col1 in range(self.cols):
-                if current_matrix[-1][col1] == 0:
+                matrix_tuple = tuple(tuple(row) for row in current_matrix)
+                if matrix_tuple in self.visited:
                     continue
-                for row1 in range(self.rows):
-                    if current_matrix[row1][col1] != 0:
-                        weight = current_matrix[row1][col1]
-                        current_matrix[row1][col1] = 0
-                        for col2 in range(self.cols):
-                            for row2 in reversed(range(self.rows)):
-                                if current_matrix[row2][col2] == 0 and col2 != col1:
-                                    current_matrix[row2][col2] = weight
-                                    print("\n--------------")
-                                    print(current_matrix[0])
-                                    print(current_matrix[1])
-                                    print("--------------\n")
-                                    matrix_tuple = tuple(tuple(row) for row in current_matrix)
-                                    if matrix_tuple in self.visited:
-                                        continue
-                                    self.visited.add(matrix_tuple)
+                self.visited.add(matrix_tuple)
+                #self.solve_current_column(current_matrix, 1)
+                for col in range(self.cols):
+                    if current_matrix[-1][col] == 0:
+                        continue
+                    self.solve_current_column(current_matrix, popped_matrix, col)
 
-                                    # Append a copy of the matrix to the queue
-                                    self.queue.append(copy.deepcopy(current_matrix))
+    def solve_current_column(self, matrix, original_matrix, col):
+        for row1 in range(self.rows):
+            if matrix[row1][col] != 0:
+                weight = matrix[row1][col]
+                print("Original Matrix:")
+                print(original_matrix)
+                print("Current Matrix:")
+                print(matrix)
 
-                                    print("Queue Size:", len(self.queue))
-                                    print("Queue Elements:")
-                                    for elem in self.queue:
-                                        print(elem)
+                print("!!!ROW:")
+                print(row1)
+                print("!!!COLUMN:")
+                print(col)
+                print("!!! WEIGHT:")
+                print(weight)
 
-                                    current_matrix[row2][col2] = 0
-                                    break  # 只跳出当前循环
+                matrix[row1][col] = 0
 
+                for col2 in range(self.cols):
+                    for row2 in reversed(range(self.rows)):
+                        if matrix[row2][col2] == 0 and col2 != col:
+                            matrix[row2][col2] = weight
+
+                            print("\n--------------")
+                            print(matrix[0])
+                            print(matrix[1])
+                            print("--------------")
+
+                            if self.is_balanced(matrix):
+                                print("FOUND SOLUTION!!!\n\n")
+
+                            matrix_tuple = tuple(tuple(row) for row in matrix)
+                            if matrix_tuple in self.visited:
+                                continue
+
+                            self.visited.add(matrix_tuple)
+                            # Append a copy of the matrix to the queue
+                            self.queue.append(copy.deepcopy(matrix))
+
+                            print("Queue Size:", len(self.queue))
+                            print("Queue Elements:")
+                            for elem in self.queue:
+                                print(elem)
+                                print()
+
+                            matrix[row2][col2] = 0
+                            break  # Exit the inner loop
+                    matrix = [row[:] for row in original_matrix]
+
+                break  # Exit the outer loop
 
     def is_balanced(self, matrix):
         left_sum = sum(matrix[i][j] for i in range(self.rows) for j in range(self.cols // 2))
         right_sum = sum(matrix[i][j] for i in range(self.rows) for j in range(self.cols // 2, self.cols))
 
-        print("Left Sum:", left_sum)
-        print("Right Sum:", right_sum)
+        # print("Left Sum:", left_sum)
+        # print("Right Sum:", right_sum)
 
         balancing_score = min(left_sum, right_sum) / max(left_sum, right_sum)
         return balancing_score > 0.9
@@ -76,6 +102,10 @@ class FindBalancingPath:
 
 
 def main():
+    # matrix = [
+    #     [6, 0, 0, 0],
+    #     [10, 4, 0, 0]
+    # ]
     matrix = [
         [6, 0, 0, 0],
         [10, 4, 0, 0]
