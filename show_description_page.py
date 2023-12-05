401196feec89661f277e5252401e580eb447abe6
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-12-04 19:57:14 -0800

    rn step 1/3, step 2/3....can be showed, but the manifest still not showing correctly

--- show_description_page.py
+++ show_description_page.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter.simpledialog import askstring  # Import askstring for input dialog
+import os
 
 class ShowDescriptions:
     def __init__(self, app):
@@ -8,14 +11,17 @@ class ShowDescriptions:
         self.descriptions = []
         self.current_step = 0
 
+        self.file_name_label = Label(self.frame, text="file_name", font=("Arial", 20), bg="white", fg="red")
+        self.file_name_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+
         self.label1 = Label(self.frame, text="", font=("Arial", 20), bg="white", justify="center")
-        self.label1.grid(row=0, column=0, padx=20, pady=20)
+        self.label1.grid(row=1, column=1, padx=20, pady=20)
 
         self.start_button = Button(self.frame, text="Next Step", font=("Arial", 18), bg="red", command=self.next_step, state=DISABLED)
-        self.start_button.grid(row=1, column=0, pady=20)
+        self.start_button.grid(row=2, column=1, pady=20)
 
         self.new_task_button = Button(self.frame, text="Start a new task", font=("Arial", 18), bg="green", command=self.start_new_task)
-        self.new_task_button.grid(row=2, column=0, pady=20)
+        self.new_task_button.grid(row=3, column=1, pady=20)
         self.new_task_button.grid_remove()  # Initially hide the "Start a new task" button
 
     def show(self):
@@ -44,7 +50,11 @@ class ShowDescriptions:
         self.app.show_page1()
 
     def update_label(self):
-        self.label1.config(text=self.descriptions[self.current_step])
+        step_info = f"Step {self.current_step + 1}/{len(self.descriptions)}"
+        description_text = self.descriptions[self.current_step]
+        combined_text = f"{step_info}\n\n{description_text}"
+
+        self.label1.config(text=combined_text)
         self.start_button.config(state=NORMAL)  # Enable the button
 
     def reset(self):
@@ -53,3 +63,12 @@ class ShowDescriptions:
         self.label1.config(text="")
         self.start_button.config(state=DISABLED)  # Disable the button initially
         self.new_task_button.grid_remove()
+
+    def update_file_name(self, file_path):
+        print(f"$$$$$Updating file name with path: {file_path}")
+        file_name = os.path.basename(file_path)
+        file_name_no_extension = os.path.splitext(file_name)[0]
+        print(f"$$$$$Updated file name: {file_name_no_extension}")
+        self.file_name_label.config(text=f"{file_name_no_extension}")
+
+
